bc4d0152e9e63659bd994ee91ae25c91f7db3b2b
Sławomir Matuszak <s.matuszak@gmail.com>
2020-03-18 22:14:26 +0100

    initial

---
+++ DE.IT.R
@@ -0,0 +1,28 @@
+de.it <- covid.chiny %>%
+  filter(liczba.ofiar>0) 
+
+de.it <- de.it %>%
+  filter(Country.Region=="Italy"|Country.Region=="Germany") %>%
+  group_by(Country.Region) %>%
+  mutate(id=row_number()-1)
+
+# liczba zgonóW we Włoszech i Niemczech
+ggplot()+
+  geom_path(data=de.it, aes(x=id, y=liczba.ofiar, color=Państwo),size=2, alpha=0.8)+
+  xlim(0,9)+
+  ylim(0,60)+
+  labs(y = "liczba zgonów", x="liczba dni od pierwszego zgonu", color=NULL)+
+  theme_bw()
+
+# śmiertelność we Włoszech i Niemczech
+ggplot()+
+  geom_path(data=de.it, aes(x=id, y=smiertelnosc, color=Państwo),size=2, alpha=0.8)+
+  geom_point(data=de.it, aes(x=id, y=smiertelnosc, color=Państwo),size=3, alpha=0.8)+
+  xlim(0,19)+
+  scale_y_continuous(labels = label_percent()) +
+  #ylim(0,60)+
+  labs(y = "śmiertelność", x="liczba dni od pierwszego zgonu", color=NULL)+
+  theme_bw()
+
+ggplot()+
+  geom_point(data=de.it, aes(x=id, y=nowe.zgony, color=Państwo),size=2, alpha=0.8)

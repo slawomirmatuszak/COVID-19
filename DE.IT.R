425414c50b07fb721940f761a7a1f866282b8b15
Sławomir Matuszak <s.matuszak@gmail.com>
2020-03-21 09:38:01 +0100

    poprawka z covid.chiny na covid. Chyba działa

--- DE.IT.R
+++ DE.IT.R
@@ -1,4 +1,5 @@
-de.it <- covid.chiny %>%
+# dałem jako źródło covid.chiny, ale chyba może być po prostu covid?
+de.it <- covid %>%
   filter(liczba.ofiar>0) 
 
 de.it <- de.it %>%
@@ -9,7 +10,7 @@ de.it <- de.it %>%
 # liczba zgonóW we Włoszech i Niemczech
 ggplot()+
   geom_path(data=de.it, aes(x=id, y=liczba.ofiar, color=Państwo),size=2, alpha=0.8)+
-  xlim(0,9)+
+  xlim(0,12)+
   ylim(0,60)+
   labs(y = "liczba zgonów", x="liczba dni od pierwszego zgonu", color=NULL)+
   theme_bw()

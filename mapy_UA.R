8440090eeea8c7146102dc7bfc45e2f51289d18d
Sławomir Matuszak <s.matuszak@gmail.com>
2020-03-27 22:57:55 +0100

    Dodanie nowych wykresów map

--- mapy_UA.R
+++ mapy_UA.R
@@ -38,24 +38,99 @@ szpitale.lozka <- szpitale.lozka %>%
   mutate(lat = as.numeric(lat), long = as.numeric(long))
 
 load(file = "E:/R/COVID-19/Ukraina.dane/szpitale.oblozenie.Rda")
-szpitale.lozka <- szpitale.oblozenie %>%
+szpitale.oblozenie <- szpitale.oblozenie %>%
   select(-c(3,4))%>%
   left_join(obwody.lista, by="Kod")%>%
   mutate(lat = as.numeric(lat), long = as.numeric(long))
 
+# procent zapełnienia łóżek
+# do poprawienia
+a <- szpitale.oblozenie %>%
+  group_by(Obwód)%>%
+  summarise(
+    lozka = sum(filter(stan=="wolne łóżka"))+sum(filter(stan=="chorzy"))
+  )
 
-### przykładowa mapa ze spisu_UA
+### liczba łóżek na 10 tys
 ggplot() + 
   geom_map(data=szpitale.lozka, aes(map_id=Kod, fill=lozka.10tys), map=shp1f) + 
   geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="grey", size=0.001) + 
   coord_map(projection = "mercator") + 
-  #geom_text(data=szpitale.lozka, aes(x=long_s, y=lat_s), label = szpitale.lozka$lozka.10tys)
   scale_fill_gradient(low = "deepskyblue2", high = "blue4") +
-  # można też geom_label, ale nie ma check_overlap label=paste(round(zmiana$proc_m, 1), "%")
-  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=paste(round(szpitale.lozka$lozka.10tys, 1)))
+  labs(fill= "liczba \nłóżek", title = "Liczba przystosowanych łóżek na 10 tysięcy mieszkańców",
+       subtitle = "stan na 26 marca") +
+  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=paste(round(szpitale.lozka$lozka.10tys, 1))) +
+  theme_bw()+
+  theme(axis.ticks = element_blank(), panel.border = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(),
+         axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.2),
+        panel.grid.minor = element_blank(),panel.grid.major = element_blank(), plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
 
+# liczba chorych
+a <- obwody %>%
+  filter(data== max(data)) %>%
+  group_by(Obwód, Kod, data)%>%
+  summarise(
+    liczba = sum(liczba)
+  )
 
-  # próbujemy dodać tytuł do wykresu. 
-  labs(fill = "Ludność\nw mln") +
-  
-  temat 
+ggplot() + 
+  geom_map(data=a, aes(map_id=Kod, fill=liczba), map=shp1f) + 
+  geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="grey", size=0.001) + 
+  coord_map(projection = "mercator") + 
+  scale_fill_gradient(low = "white", high = "orange") +
+  labs(fill= "liczba \nzarażeń", title = "Liczba potwierdzonych przypadków Covid-19",
+       subtitle =  paste( "stan na", format(as.Date(a$data), "%d/%m/%Y"))) +
+  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=a$liczba) +
+  theme_bw()+
+  theme(axis.ticks = element_blank(), panel.border = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.2),
+        panel.grid.minor = element_blank(),panel.grid.major = element_blank(), plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
+
+# liczba zgonóW
+a <- obwody %>%
+  filter(data== max(data)) %>%
+  filter(skumulowane == "zgony") %>%
+  group_by(Obwód, Kod, data)%>%
+  summarise(
+    liczba = sum(liczba)
+  )
+
+ggplot() + 
+  geom_map(data=a, aes(map_id=Kod, fill=liczba), map=shp1f) + 
+  geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="grey", size=0.001) + 
+  coord_map(projection = "mercator") + 
+  scale_fill_gradient(low = "white", high = "red") +
+  labs(fill= "liczba \nzgonów", title = "Liczba zgonów z powodu Covid-19",
+       subtitle =  paste( "stan na", format(as.Date(a$data), "%d/%m/%Y")),
+       caption = "Źródło - Ministerstwo Zdrowia Ukrainy") +
+  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=a$liczba) +
+  theme_bw()+
+  theme(axis.ticks = element_blank(), panel.border = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.2),
+        panel.grid.minor = element_blank(),panel.grid.major = element_blank(), plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
+
+# nowe przypadki
+a <- obwody %>%
+  filter(data== max(data)) %>%
+  group_by(Obwód, Kod, data)%>%
+  summarise(
+    ilosc = sum(ilosc)
+  )
+
+ggplot() + 
+  geom_map(data=a, aes(map_id=Kod, fill=ilosc), map=shp1f) + 
+  geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="grey", size=0.001) + 
+  coord_map(projection = "mercator") + 
+  scale_fill_gradient(low = "white", high = "orange") +
+  labs(fill= "liczba nowych\nprzypadków", title = "Liczba nowych przypadków Covid-19 w ciągu poprzedniej doby",
+       subtitle =  paste( "stan na", format(as.Date(a$data), "%d/%m/%Y")),
+       caption = "Źródło - Ministerstwo Zdrowia Ukrainy") +
+  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=a$ilosc) +
+  theme_bw()+
+  theme(axis.ticks = element_blank(), panel.border = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.2),
+        panel.grid.minor = element_blank(),panel.grid.major = element_blank(), plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))

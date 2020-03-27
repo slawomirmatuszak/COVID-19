fb3896cb5b770a99d26574016e96cd9beb971a49
Sławomir Matuszak <s.matuszak@gmail.com>
2020-03-27 17:46:12 +0100

    dodanie długości i szerokości do obwodów (nie stolicznej)

--- mapy_UA.R
+++ mapy_UA.R
@@ -2,7 +2,6 @@ library(maptools)
 library(rgeos)
 library(rgdal)
 library(ggplot2)
-library(stringr)
 library(gridExtra)
 library(reshape2)
 library(ggthemes)
@@ -19,26 +18,43 @@ shp1 <- readOGR("E:/R/UA_spis/data/mapa", layer = "obwod_pl")
 #zmieniamy format danych
 shp1f <- fortify(shp1, region = "ADM1_PCODE")
 
-## pobieramy dane o obwodach
-obwody <- read.csv("E:/R/UA_spis/data/obwody.csv", encoding = "UTF-8")
-names(obwody)[1] <- "id"
-names(obwody)[2] <- "Область"
-# usuwamy niepotrzebne kolumny
-obwody <- tbl_df(obwody)
-obwody <- select(obwody, -c(11:17))
 
+# dzienne dane z obwodów i szpitali
+load(file = "E:/R/COVID-19/Ukraina.dane/obwody.lista.Rda")
+obwody.lista <- obwody %>%
+  select(1:3,7)
+rm(obwody)
+
+load(file = "E:/R/COVID-19/Ukraina.dane/obwody_dzienne.Rda")
+obwody <- obwody %>%
+  select(-c(7,8))%>%
+  left_join(obwody.lista, by="Kod")%>%
+  mutate(lat = as.numeric(lat), long = as.numeric(long))
+
+load(file = "E:/R/COVID-19/Ukraina.dane/szpitale.lozka.Rda")
+szpitale.lozka <- szpitale.lozka %>% 
+  select(-c(6,7))%>%
+  left_join(obwody.lista, by="Kod")%>%
+  mutate(lat = as.numeric(lat), long = as.numeric(long))
+
+load(file = "E:/R/COVID-19/Ukraina.dane/szpitale.oblozenie.Rda")
+szpitale.lozka <- szpitale.oblozenie %>%
+  select(-c(3,4))%>%
+  left_join(obwody.lista, by="Kod")%>%
+  mutate(lat = as.numeric(lat), long = as.numeric(long))
 
 
 ### przykładowa mapa ze spisu_UA
 ggplot() + 
-  ggtitle("Ilość mieszkańców wg. spisu Dubileta") +
-  geom_map(data=spis, aes(map_id=id, fill=r.2020.dubilet/1000000), map=shp1f) + 
-  geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="grey", size=0.01) + 
+  geom_map(data=szpitale.lozka, aes(map_id=Kod, fill=lozka.10tys), map=shp1f) + 
+  geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="grey", size=0.001) + 
   coord_map(projection = "mercator") + 
+  #geom_text(data=szpitale.lozka, aes(x=long_s, y=lat_s), label = szpitale.lozka$lozka.10tys)
   scale_fill_gradient(low = "deepskyblue2", high = "blue4") +
-  
   # można też geom_label, ale nie ma check_overlap label=paste(round(zmiana$proc_m, 1), "%")
-  geom_label(data=spis, aes(x=long, y=lat), label=paste(round(spis$`r.2020.dubilet`/1000000,2), "mln"), nudge_y=-0.1, size=5) +
+  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=paste(round(szpitale.lozka$lozka.10tys, 1)))
+
+
   # próbujemy dodać tytuł do wykresu. 
   labs(fill = "Ludność\nw mln") +
   

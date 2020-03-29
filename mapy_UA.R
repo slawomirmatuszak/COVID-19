91b0356a956ec15200d61e6ddeec500471555364
Sławomir Matuszak <s.matuszak@gmail.com>
2020-03-29 15:18:24 +0200

    Wykres zachorowań na 100 tys mieszkańców

--- mapy_UA.R
+++ mapy_UA.R
@@ -22,7 +22,10 @@ shp1f <- fortify(shp1, region = "ADM1_PCODE")
 # dzienne dane z obwodów i szpitali
 load(file = "E:/R/COVID-19/Ukraina.dane/obwody.lista.Rda")
 obwody.lista <- obwody %>%
+  # trzeba poprawic współrzędne Kijowa, bo nie widać go na mapie
   select(1:3,7)
+obwody.lista[13,1] <- 50.777435
+obwody.lista[13,2] <- 30.167475
 rm(obwody)
 
 load(file = "E:/R/COVID-19/Ukraina.dane/obwody_dzienne.Rda")
@@ -137,3 +140,32 @@ ggplot() +
         axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.2),
         panel.grid.minor = element_blank(),panel.grid.major = element_blank(), plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
+
+## liczba przypadków na 100 tys mieszkańców
+a <- obwody %>%
+  filter(data== max(data)) %>%
+  group_by(Obwód, Kod, data, population)%>%
+  summarise(
+    liczba = sum(liczba),
+    data2 = max(data)+1
+  ) %>%
+  mutate(zach.100 = liczba*100000/population)%>%
+  mutate(zach.100 = round(zach.100, 2))
+
+png("100tys.png", units="in", width=11, height=6, res=600)
+ggplot() + 
+  geom_map(data=a, aes(map_id=Kod, fill=zach.100), map=shp1f) + 
+  geom_path(data = shp1f, aes(x=long, y=lat, group=group), colour="black", size=0.001) + 
+  coord_map(projection = "mercator") + 
+  scale_fill_gradient(low = "white", high = "orange") +
+  labs(fill= "liczba\nzarażonych", 
+       title = "Liczba zarażonych Covid-19 na 100 tys. mieszkańców",
+       subtitle =  paste( "stan na", format(as.Date(a$data2), "%d/%m/%Y")),
+       caption = "Źródło - Ministerstwo Zdrowia Ukrainy, liczba ludności wg tzw. spisu Dubileta") +
+  geom_label(data=szpitale.lozka, aes(x=long, y=lat), label=a$zach.100) +
+  theme_bw()+
+  theme(axis.ticks = element_blank(), panel.border = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = c(0.1, 0.2),
+        panel.grid.minor = element_blank(),panel.grid.major = element_blank(), plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
+dev.off()

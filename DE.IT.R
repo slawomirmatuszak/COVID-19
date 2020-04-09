3b0df6b5c83639c541ce417f467477e106e0ee24
Sławomir Matuszak <s.matuszak@gmail.com>
2020-04-09 15:26:40 +0200

    dodanie Francji i Hiszpanii

--- DE.IT.R
+++ DE.IT.R
@@ -10,8 +10,8 @@ de.it <- de.it %>%
 # liczba zgonóW we Włoszech i Niemczech
 ggplot()+
   geom_path(data=de.it, aes(x=id, y=liczba.ofiar, color=Państwo),size=2, alpha=0.8)+
-  xlim(0,12)+
-  ylim(0,60)+
+  xlim(0,30)+
+  ylim(0,6000)+
   labs(y = "liczba zgonów", x="liczba dni od pierwszego zgonu", color=NULL)+
   theme_bw()
 
@@ -19,7 +19,7 @@ ggplot()+
 ggplot()+
   geom_path(data=de.it, aes(x=id, y=smiertelnosc, color=Państwo),size=2, alpha=0.8)+
   geom_point(data=de.it, aes(x=id, y=smiertelnosc, color=Państwo),size=3, alpha=0.8)+
-  xlim(0,19)+
+  xlim(0,30)+
   scale_y_continuous(labels = label_percent()) +
   #ylim(0,60)+
   labs(y = "śmiertelność", x="liczba dni od pierwszego zgonu", color=NULL)+
@@ -27,3 +27,29 @@ ggplot()+
 
 ggplot()+
   geom_point(data=de.it, aes(x=id, y=nowe.zgony, color=Państwo),size=2, alpha=0.8)
+
+duze.UE <- covid %>%
+  filter(liczba.ofiar>50) 
+
+duze.UE <- duze.UE %>%
+  filter(Country.Region=="Italy"|Country.Region=="Germany"|Country.Region=="France"|Country.Region=="Spain") %>%
+  group_by(Country.Region) %>%
+  mutate(id=row_number()-1)
+
+# liczba zgonóW 
+ggplot()+
+  geom_path(data=duze.UE, aes(x=id, y=liczba.ofiar, color=Państwo),size=2, alpha=0.8)+
+  coord_cartesian(xlim=c(0,20), ylim=c(0,8000))+
+  labs(y = "liczba zgonów", x="liczba dni od 50-tego zgonu", color=NULL)+
+  theme_bw()+
+  theme(legend.position = "top")
+
+# śmiertelność 
+ggplot()+
+  geom_path(data=duze.UE, aes(x=id, y=smiertelnosc, color=Państwo),size=2, alpha=0.8)+
+  geom_point(data=duze.UE, aes(x=id, y=smiertelnosc, color=Państwo),size=3, alpha=0.8)+
+  coord_cartesian(xlim=c(0,30))+
+  scale_y_continuous(labels = label_percent()) +
+  labs(y = "śmiertelność", x="liczba dni od 50-tego zgonu", color=NULL)+
+  theme_bw()+
+  theme(legend.position = "top")
